a32bdf9e7ece9f3f5eeb01785a371e971e52377b
Fakunabs <gempro0501@gmail.com>
2023-01-06 10:21:27 +0700

    From Mac

--- Sophomore/Java/Java First Project/src/Input.java
+++ Sophomore/Java/Java First Project/src/Input.java
@@ -2,8 +2,11 @@ public class Input {
     public static void main(String[] args) {
         String thongBao = "Xin chào!";
         String hoVaTen = "Nguyễn Xuân Thịnh";
-        String tenSach = "Lap trinh Java";
+        String tenSach;
+        tenSach = "Lap trinh Java";
 
         System.out.println("Thong bao: "+ thongBao);
+        System.out.println("Ho va Ten: "+ hoVaTen);
+        System.out.println("Ten sach: "+ tenSach);
     }
 }
---
+++ Sophomore/Java/Java First Project/src/ViDu.java
@@ -0,0 +1,14 @@
+public class ViDu {
+    public static void main(String[] args) {
+        String tenSach = "Lap trinh Java";
+        int namXuatBan = 2021;
+        double giaTien = 15.5;
+        boolean conSach = true;
+        char maKho = 'M';
+        System.out.println("Ten sach: " + tenSach);
+        System.out.println("Nam xuat ban: " + giaTien);
+        System.out.println("Gia tien: " + giaTien);
+        System.out.println("Tinh trang sach: " + conSach);
+        System.out.println("Ma kho: " + maKho);
+    }
+}
