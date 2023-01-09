4a299ca0aafa310706a04c2780819823c7518b2d
Fakunabs <gempro0501@gmail.com>
2023-01-09 17:34:22 +0700

    From Mac

--- Sophomore/Java/Java First Project/src/chepPhat.java
+++ Sophomore/Java/Java First Project/src/chepPhat.java
@@ -1,11 +1,11 @@
+import java.util.Scanner;
+
 public class chepPhat {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10 ; i++) {
-            System.out.println(i + ". Xin chao");
-        }
-        System.out.println("\n");
-        for (int i = 10; i > 0 ; i--) {
-            System.out.println(i + ". Tam biet");
-        }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Nhap vao so nguyen n: ");
+        n = sc.nextInt();
+        System.out.println();
     }
 }
