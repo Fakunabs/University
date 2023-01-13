a017d037d51d4aac4680534206143d205121ffd7
Fakunabs <gempro0501@gmail.com>
2023-01-13 20:41:15 +0700

    From Mac

--- Code/Code CTDL/23-11-2022.cpp
+++ Code/Code CTDL/23-11-2022.cpp
@@ -15,3 +15,4 @@ struct SinhVien
 
 typedef struct sinhvien SV;
 
+
---
+++ Sophomore/Java/Java First Project/src/CaPhe/HoaDonCaPhe.java
@@ -0,0 +1,13 @@
+package CaPhe;
+
+public class HoaDonCaPhe {
+    private String tenLoaiCaPhe;
+    private double giaTien1Kg;
+    private double khoiLuong;
+
+    public HoaDonCaPhe(String tenLoaiCaPhe, double giaTien1Kg, double khoiLuong) {
+        this.tenLoaiCaPhe = ;
+        this.giaTien1Kg = giaTien1Kg;
+        this.khoiLuong = khoiLuong;
+    }
+}
